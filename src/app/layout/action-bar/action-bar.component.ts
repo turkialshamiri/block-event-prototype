b41ed600f010543e97731a0f281f666fb63d4665
turkialshamiri <turkimohammedalshamiri@gmail.com>
2026-06-24 17:55:52 +0300

    complete the part of event

--- src/app/layout/action-bar/action-bar.component.ts
+++ src/app/layout/action-bar/action-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { LayoutUiService } from '../../core/layout-ui.service';
 
 @Component({
   selector: 'app-action-bar',
@@ -6,6 +7,10 @@ import { Component, output } from '@angular/core';
   styleUrl: './action-bar.component.scss',
 })
 export class ActionBarComponent {
+  private readonly layoutUi = inject(LayoutUiService);
+
+  protected readonly showEventWorkspace = this.layoutUi.showEventWorkspace;
+
   readonly createEvent = output<void>();
 
   protected onCreateEvent(): void {
